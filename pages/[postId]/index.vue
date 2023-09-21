31dfb9f5ad856b5a56a965cbf809f4797ee9502a
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2023-09-22 04:07:58 +0900

    Initial commit

---
+++ pages/[postId]/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <Suspense>
+    <main>
+      <h1>{{ article.title }}</h1>
+      <p>
+        <time :datetime="article.publishedAt" v-text="article.publishedAt" />
+      </p>
+      <div v-html="article.content" />
+    </main>
+  </Suspense>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+const postId = route.params.postId;
+const { data: article } = await useFetch(`/blog/${slug}`, {
+  baseURL: 'https://tiamat.microcms.io/api/v1',
+  headers: {
+    'X-MICROCMS-API-KEY': 'cea0bdf6bfc54fbd8b53f3bfbbdc346710d8',
+  },
+});
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const { data: posts, error } = await useFetch('/blog', {
+  baseURL: 'https://tiamat.microcms.io/api/v1',
+  headers: {
+    'X-MICROCMS-API-KEY': 'cea0bdf6bfc54fbd8b53f3bfbbdc346710d8',
+  },
+});
+</script>
+
+<template>
+  <div>
+    <h3>MicroCMS＋Nuxt3 SSG</h3>
+    <ol>
+      <li v-for="article in posts?.contents" :key="artivle.id">
+        <nuxt-link :to="`/${article.id}`">{{ article.title }}</nuxt-link>
+        {{ console.log(article) }}
+      </li>
+    </ol>
+  </div>
+</template>
